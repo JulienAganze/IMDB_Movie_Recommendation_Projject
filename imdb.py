c3e41f18f4d37badc3afa78f2b51c04ee939a30a
JulienAganze <aganzejulien2017@gmail.com>
2023-04-24 17:00:58 +0200

    movie titles and names both accessed here

--- imdb.py
+++ imdb.py
@@ -1,4 +1,22 @@
 # %%
 import requests
-res =requests.get('https://www.imdb.com/chart/top/')
-print(res.text)
+from bs4 import BeautifulSoup
+url = 'https://www.imdb.com/chart/top/'
+headers = {"Accept-Language": "en-US,en;q=0.5"}
+res =requests.get(url,headers=headers)
+html = res.text
+soup = BeautifulSoup(html,'html.parser')
+for tag in soup.findAll('img'):
+    print(tag['alt'])
+
+# %%
+import requests
+from bs4 import BeautifulSoup
+url = 'https://www.imdb.com/chart/top/'
+headers = {"Accept-Language": "en-US,en;q=0.5"}
+res =requests.get(url,headers=headers)
+html = res.text
+soup = BeautifulSoup(html,'html.parser')
+for tag in soup.findAll('span',{'class':'secondaryInfo'}):
+    print(tag.string.replace('(','').replace(')',''))
+
